097e042f29140c9334e36207e9fcf2f1f6c219fd
chenxiong <1045136563@qq.com>
2015-10-30 16:55:11 +0800

    格式化代码，添加注释

--- wx_menu.php
+++ wx_menu.php
@@ -1,26 +1,27 @@
 <?php
 //登格帝微信公众平台菜单
-$appid = "wx595da41455e1b3d7";
-$appsecret = "b07b81371fd562be49b810b5eb37a4db";
-$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret";
+$appid        = "wx595da41455e1b3d7";//微信公众平台 appid
+$appsecret    = "b07b81371fd562be49b810b5eb37a4db";//微信公众平台 appid
+$url          = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret";
 
-$output = https_request($url);
-$jsoninfo = json_decode($output, true);
+$output       = https_request($url);
+$jsoninfo     = json_decode($output, true);
 $access_token = $jsoninfo["access_token"];
 
-$jsonmenu = '{
-      "button":[
-      {
-        "type":"view",
-        "name":"微商城",
-        "url":"http://dgd.hanjiajobs.com/mobile/"
-      }]
- }';
+$jsonmenu 	  = '{
+				      "button":[
+				      {
+				        "type":"view",
+				        "name":"微商城",
+				        "url":"http://dgd.hanjiajobs.com/mobile/"
+				      }]
+				 }';
 
-$url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
-$result = https_request($url, $jsonmenu);
+$url    	  = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
+$result 	  = https_request($url, $jsonmenu);
 var_dump($result);
 
+/*获取https请求内容*/
 function https_request($url,$data = null){
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
